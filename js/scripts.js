486fb5d0a1c71b1caa259e9ed6b6cd616746c711
Kai Kohlsdorf <kai.w.kohlsdorf@gmail.com>
2022-06-03 11:14:01 -0700

    Add submit button, quiz and results containers, fix errors, and add initial functions.

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,19 @@
-$(document).ready(function() {
-  const animal = $("input:radio[name=animal]:checked").val();
+function buildQuiz(){}
 
+function showResults(){}
+
+
+$(document).ready(function() {
+
+  const animaltypel= $("input:radio[name=animal]:checked").val();
+  const costumetype= $("input:radio[name=costume]:checked").val();
+  const beveragetype= $("input:radio[name=costume]:checked").val();
+  const subjecttype= $("input:radio[name=subject]:checked").val();
+  const purchasetype= $("input:radio[name=purchase]:checked").val();
+  
+  buildQuiz();
+
+  submitButton.addEventListener('click', showResults);
+  
+  
 })
