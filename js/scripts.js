f4ea679d324277f544b2769f21f86ce7f5cabe34
Kai Kohlsdorf <kai.w.kohlsdorf@gmail.com>
2022-06-03 15:54:44 -0700

    Stop trying to collate into 'mostly a' 'mostly b' and 'mostly c' categories and return to simpler prompt.

--- js/scripts.js
+++ js/scripts.js
@@ -4,30 +4,25 @@ function showResults(){}
 
 
 $(document).ready(function() {
-
-  const animaltypel= $("input:radio[name=animal]:checked").val();
-  const costumetype= $("input:radio[name=costume]:checked").val();
-  const beveragetype= $("input:radio[name=costume]:checked").val();
-  const subjecttype= $("input:radio[name=subject]:checked").val();
-  const purchasetype= $("input:radio[name=purchase]:checked").val();
-
+  
+  const animal= $("input:radio[name=animal]:checked").val();
+  const costume= $("input:radio[name=costume]:checked").val();
+  const beverage= $("input:radio[name=costume]:checked").val();
+  const subject= $("input:radio[name=subject]:checked").val();
+  const purchase= $("input:radio[name=purchase]:checked").val();
+    if (a >= 3) {
+      $('#mostlya').show();
+    } else if (a < 3 && b >= 3) {
+      $('#mostlyb').show();
+    } else {
+      $('#mostlyc').show();
+    }
   buildQuiz();
 
   $("p").click(function() {
     $("results").show();
     $("results").hide();
   });
-
-
-  const value = "mostlya" ("Submit");
-
-    if (value = mostlya) {
-      $('#mostlya').show();
-    } else if (PLACEHOLDER) {
-      $('#mostlyb').show();
-    } else {
-      $('#mostlyc').show();
-    }
   
   
 });  
