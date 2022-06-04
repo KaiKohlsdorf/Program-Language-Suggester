bec6f66eb8c41e0b1d1958ed5b8ceba693562588
Kai Kohlsdorf <kai.w.kohlsdorf@gmail.com>
2022-06-03 17:11:39 -0700

    Add README.md information and license, update div id, branch initial question when go to site, and WIP on getting site to load on if else.

---
+++ README.md
@@ -0,0 +1,38 @@
+# Program Language Suggester
+
+#### Kai's Program Language Suggester is a silly quiz you can take to see which code Kai thinks you should start with on your coding journey.
+
+## Contributor: Kai Kohlsdorf
+
+## Technologies Used
+
+*HTML
+
+*CSS
+
+*Javascript
+
+*jQuery
+
+*Bootstrap
+
+*Markdown
+
+## Installation
+
+*Clone this repository to your desktop.
+
+*Navigate to the top level of the directory.
+
+*Open Program-Language-Suggester/index.html in your browser.
+
+## Known Bugs: 
+
+*Submit button does not work.
+
+*Currently no answers bring up the quiz.
+
+## License
+[MIT][<a href=https://github.com/KaiKohlsdorf/Program-Language-Suggester/blob/main/LICENSE>LICENSE</a>]
+
+Copyright (c) _June 3, 2022_ _Kai Kohlsdorf_
--- js/scripts.js
+++ js/scripts.js
@@ -1,29 +1,22 @@
-function buildQuiz(){}
-
-function showResults(){}
-
-
 $(document).ready(function() {
+  const intro= parseInt(prompt("Type 'yes' if you want to learn to code!"));
+
+    if (intro = yes || Yes || YES) {
+    $('#yes').show();
+    } else {
+    $('#no').show();
+    }
+
   
   const animal= $("input:radio[name=animal]:checked").val();
   const costume= $("input:radio[name=costume]:checked").val();
   const beverage= $("input:radio[name=costume]:checked").val();
   const subject= $("input:radio[name=subject]:checked").val();
   const purchase= $("input:radio[name=purchase]:checked").val();
-    if (a >= 3) {
-      $('#mostlya').show();
-    } else if (a < 3 && b >= 3) {
-      $('#mostlyb').show();
-    } else {
-      $('#mostlyc').show();
-    }
-  buildQuiz();
+    
 
   $("p").click(function() {
     $("results").show();
     $("results").hide();
   });
-  
-  
-});  
-
+}); 
